54078779c5200d8b71760bad7b123e618c67c82f
0GiS0 <giselatb@outlook.com>
2023-04-11 15:46:37 +0200

    add capacitor for android support

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: '',
+  apiUrl: 'https://tour-of-heroes-api-spring-tour-of-heroes-api.azuremicroservices.io/api/hero',
   appInsights: {
-    connectionString: '[Replace With Your Key]'
+    connectionString: 'InstrumentationKey=d9572e19-f6c1-4794-af90-f403e98adf77;IngestionEndpoint=https://northeurope-0.in.applicationinsights.azure.com/;LiveEndpoint=https://northeurope.livediagnostics.monitor.azure.com/'
   }
 };
