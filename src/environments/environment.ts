dc2beee6a2465da55fb1ffa74258ba722d87790e
Gisela <giselatb@outlook.com>
2023-04-12 10:30:04 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080/api/hero',
   appInsights: {
-    connectionString: 'InstrumentationKey=d9572e19-f6c1-4794-af90-f403e98adf77;IngestionEndpoint=https://northeurope-0.in.applicationinsights.azure.com/;LiveEndpoint=https://northeurope.livediagnostics.monitor.azure.com/'
+    connectionString: '[Your_App_Insights_Connection_String_Here]'
   }
 };
 
