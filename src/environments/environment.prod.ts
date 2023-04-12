76c453d82c1c159acd895d5a4a85ac31e176a7d7
Gisela <giselatb@outlook.com>
2023-04-12 10:29:44 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://tour-of-heroes-api-spring-tour-of-heroes-api.azuremicroservices.io/api/hero',
   appInsights: {
-    connectionString: 'InstrumentationKey=d9572e19-f6c1-4794-af90-f403e98adf77;IngestionEndpoint=https://northeurope-0.in.applicationinsights.azure.com/;LiveEndpoint=https://northeurope.livediagnostics.monitor.azure.com/'
+    connectionString: '[Your_App_Insights_Connection_String_Here]'
   }
 };
